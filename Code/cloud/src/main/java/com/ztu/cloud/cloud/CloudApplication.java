91f90dc8978cb454b6b91f9f4062ede158beff7b
Jager <32773276+secludewolf@users.noreply.github.com>
2020-07-17 09:15:22 +0800

    解决依赖冲突

--- Code/cloud/src/main/java/com/ztu/cloud/cloud/CloudApplication.java
+++ Code/cloud/src/main/java/com/ztu/cloud/cloud/CloudApplication.java
@@ -2,6 +2,7 @@ package com.ztu.cloud.cloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 /**
  * @author Jager
@@ -9,7 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @date 2020/06/21-11:49
  **/
 @SpringBootApplication
-public class CloudApplication {
+public class CloudApplication  extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(CloudApplication.class, args);
--- Code/cloud/src/main/java/com/ztu/cloud/cloud/util/EmailUtil.java
+++ Code/cloud/src/main/java/com/ztu/cloud/cloud/util/EmailUtil.java
@@ -16,38 +16,38 @@ import javax.mail.internet.MimeMessage;
 @Component
 public class EmailUtil {
     JavaMailSender mailSender;
-
+    //TODO 暂时关闭 javaee-web-api会导致打包冲突
     public EmailUtil(JavaMailSender mailSender) {
         this.mailSender = mailSender;
     }
 
     @Async
     public void sendPasswordToEmail(String email, String password) {
-        MimeMessage message = this.mailSender.createMimeMessage();
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom("校园云盘<814878826@qq.com>");
-            helper.setTo(email);
-            helper.setSubject("校园云盘:您的新密码");
-            helper.setText("新密码:" + password + "<br>请第一时间修改密码", true);
-            this.mailSender.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
+        // MimeMessage message = this.mailSender.createMimeMessage();
+        // try {
+        //     MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        //     helper.setFrom("校园云盘<814878826@qq.com>");
+        //     helper.setTo(email);
+        //     helper.setSubject("校园云盘:您的新密码");
+        //     helper.setText("新密码:" + password + "<br>请第一时间修改密码", true);
+        //     this.mailSender.send(message);
+        // } catch (MessagingException e) {
+        //     e.printStackTrace();
+        // }
     }
 
     @Async
     public void sendRegisterSuccess(String email, String account) {
-        MimeMessage message = this.mailSender.createMimeMessage();
-        try {
-            MimeMessageHelper helper = new MimeMessageHelper(message, true);
-            helper.setFrom("校园云盘<814878826@qq.com>");
-            helper.setTo(email);
-            helper.setSubject("校园云盘:您已成功注册校园云盘");
-            helper.setText("账号:" + account, true);
-            this.mailSender.send(message);
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
+        // MimeMessage message = this.mailSender.createMimeMessage();
+        // try {
+        //     MimeMessageHelper helper = new MimeMessageHelper(message, true);
+        //     helper.setFrom("校园云盘<814878826@qq.com>");
+        //     helper.setTo(email);
+        //     helper.setSubject("校园云盘:您已成功注册校园云盘");
+        //     helper.setText("账号:" + account, true);
+        //     this.mailSender.send(message);
+        // } catch (MessagingException e) {
+        //     e.printStackTrace();
+        // }
     }
 }
