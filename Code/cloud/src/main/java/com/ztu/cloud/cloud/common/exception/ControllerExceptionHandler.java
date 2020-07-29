f1cd4fbf84affbf68f94b9113d3330feff709941
Jager <32773276+secludewolf@users.noreply.github.com>
2020-07-29 16:47:44 +0800

    增加Token异常错误全局处理

--- Code/cloud/src/main/java/com/ztu/cloud/cloud/common/exception/ControllerExceptionHandler.java
+++ Code/cloud/src/main/java/com/ztu/cloud/cloud/common/exception/ControllerExceptionHandler.java
@@ -2,6 +2,7 @@ package com.ztu.cloud.cloud.common.exception;
 
 import com.ztu.cloud.cloud.common.constant.ResultConstant;
 import com.ztu.cloud.cloud.common.vo.ResultResponseEntity;
+import io.jsonwebtoken.JwtException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -16,5 +17,11 @@ public class ControllerExceptionHandler {
 	public ResultResponseEntity missingRequestHeaderException(Exception e) {
 		e.printStackTrace();
 		return ResultConstant.REQUEST_PARAMETER_ERROR;
+	}
+
+	@ExceptionHandler(JwtException.class)
+	public ResultResponseEntity tokenIllegal(Exception e) {
+		e.printStackTrace();
+		return ResultConstant.TOKEN_INVALID;
 	}
 }
