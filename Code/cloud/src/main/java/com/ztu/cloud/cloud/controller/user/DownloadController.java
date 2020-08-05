4212c9353e9038e95ec874c9b2d859cda732813e
Jager <32773276+secludewolf@users.noreply.github.com>
2020-08-05 11:34:11 +0800

    修复错误

--- Code/cloud/src/main/java/com/ztu/cloud/cloud/controller/user/DownloadController.java
+++ Code/cloud/src/main/java/com/ztu/cloud/cloud/controller/user/DownloadController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import javax.validation.constraints.NotBlank;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -22,71 +23,74 @@ import java.net.URLEncoder;
  **/
 @RestController
 public class DownloadController {
-	DownloadService downloadService;
+    DownloadService downloadService;
 
-	public DownloadController(DownloadService downloadService) {
-		this.downloadService = downloadService;
-	}
+    public DownloadController(DownloadService downloadService) {
+        this.downloadService = downloadService;
+    }
 
-	/**
-	 * 获取下载ID
-	 *
-	 * @param token 用户Token
-	 * @param data  请求参数
-	 *              shareId 分享ID
-	 *              repositoryId 仓库ID
-	 *              fileId 文件Id
-	 *              fileName 文件名
-	 *              folder 文件夹
-	 * @return 下载ID
-	 */
-	@PostMapping("/download")
-	public ResultResponseEntity getDownloadId(@RequestHeader(TokenUtil.TOKEN_HEADER) @NotBlank(message = "Token不能为空") String token,
-	                                          @RequestBody DownloadId parameter) {
-		return this.downloadService.getDownloadId(token, parameter);
-	}
+    /**
+     * 获取下载ID
+     *
+     * @param token
+     *            用户Token
+     * @param parameter
+     *            请求参数 shareId 分享ID repositoryId 仓库ID fileId 文件Id fileName 文件名 folder 文件夹
+     * @return 下载ID
+     */
+    @PostMapping("/download")
+    public ResultResponseEntity getDownloadId(
+        @RequestHeader(TokenUtil.TOKEN_HEADER) @NotBlank(message = "Token不能为空") String token,
+        @RequestBody @Valid DownloadId parameter) {
+        return this.downloadService.getDownloadId(token, parameter);
+    }
 
-	/**
-	 * 下载文件
-	 *
-	 * @param response   response
-	 * @param downloadId 下载ID
-	 */
-	@GetMapping("/download/{downloadId}")
-	public void download(HttpServletResponse response, @PathVariable @NotBlank(message = "下载ID不能为空") String downloadId) {
-		Download download = this.downloadService.download(downloadId);
-		if (download == null) {
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("application/json");
-			try {
-				ServletOutputStream outputStream = response.getOutputStream();
-				//TODO改为JSON字符串
-				String content = "下载失效！";
-				outputStream.write(content.getBytes());
-				outputStream.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return;
-		}
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("application/force-download");
-		try {
-			response.setHeader("Content-Disposition", "attachment; fileName=" + URLEncoder.encode(download.getFileName(), "UTF-8") + ";filename*=utf-8''" + URLEncoder.encode(download.getFileName(), "UTF-8"));
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		byte[] buffer = new byte[1024];
-		try {
-			OutputStream os = response.getOutputStream();
-			int i = download.getInputStream().read(buffer);
-			while (i != -1) {
-				os.write(buffer, 0, i);
-				i = download.getInputStream().read(buffer);
-			}
-			os.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * 下载文件
+     *
+     * @param response
+     *            response
+     * @param downloadId
+     *            下载ID
+     */
+    @GetMapping("/download/{downloadId}")
+    public void download(HttpServletResponse response,
+        @PathVariable @NotBlank(message = "下载ID不能为空") String downloadId) {
+        Download download = this.downloadService.download(downloadId);
+        if (download == null) {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json");
+            try {
+                ServletOutputStream outputStream = response.getOutputStream();
+                // TODO改为JSON字符串
+                String content = "下载失效！";
+                outputStream.write(content.getBytes());
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/force-download");
+        try {
+            response.setHeader("Content-Disposition",
+                "attachment; fileName=" + URLEncoder.encode(download.getFileName(), "UTF-8") + ";filename*=utf-8''"
+                    + URLEncoder.encode(download.getFileName(), "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        byte[] buffer = new byte[1024];
+        try {
+            OutputStream os = response.getOutputStream();
+            int i = download.getInputStream().read(buffer);
+            while (i != -1) {
+                os.write(buffer, 0, i);
+                i = download.getInputStream().read(buffer);
+            }
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
