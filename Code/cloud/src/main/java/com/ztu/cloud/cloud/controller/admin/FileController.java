b9f0646cf3a9cf0f087a4e6f38346a8fc5b4f75d
Jager <32773276+secludewolf@users.noreply.github.com>
2020-08-05 15:06:17 +0800

    将FileController参数验证替换为自动验证

--- Code/cloud/src/main/java/com/ztu/cloud/cloud/controller/admin/FileController.java
+++ Code/cloud/src/main/java/com/ztu/cloud/cloud/controller/admin/FileController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
@@ -22,91 +24,111 @@ import java.net.URLEncoder;
 @RestController
 @RequestMapping("/admin")
 public class FileController {
-	FileService fileService;
+    FileService fileService;
 
-	public FileController(FileService fileService) {
-		this.fileService = fileService;
-	}
+    public FileController(FileService fileService) {
+        this.fileService = fileService;
+    }
 
-	/**
-	 * 下载文件
-	 *
-	 * @param request  request
-	 * @param response response
-	 * @param token    管理员Token
-	 * @param fileId   文件ID
-	 */
-	@GetMapping("/download/{fileId}")
-	public void download(HttpServletRequest request, HttpServletResponse response, @RequestHeader(TokenUtil.TOKEN_HEADER) String token, @PathVariable String fileId) {
-		Download download = this.fileService.download(token, fileId);
-		if (download == null) {
-			response.setCharacterEncoding("UTF-8");
-			response.setContentType("application/json");
-			try {
-				ServletOutputStream outputStream = response.getOutputStream();
-				//TODO改为JSON字符串
-				String content = "下载失效！";
-				outputStream.write(content.getBytes());
-				outputStream.close();
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-			return;
-		}
-		response.setCharacterEncoding("UTF-8");
-		response.setContentType("application/force-download");
-		try {
-			response.setHeader("Content-Disposition", "attachment; fileName=" + URLEncoder.encode(download.getFileName(), "UTF-8") + ";filename*=utf-8''" + URLEncoder.encode(download.getFileName(), "UTF-8"));
-		} catch (UnsupportedEncodingException e) {
-			e.printStackTrace();
-		}
-		byte[] buffer = new byte[1024];
-		try {
-			OutputStream os = response.getOutputStream();
-			int i = download.getInputStream().read(buffer);
-			while (i != -1) {
-				os.write(buffer, 0, i);
-				i = download.getInputStream().read(buffer);
-			}
-			os.close();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    /**
+     * 下载文件
+     *
+     * @param request
+     *            request
+     * @param response
+     *            response
+     * @param token
+     *            管理员Token
+     * @param fileId
+     *            文件ID
+     */
+    @GetMapping("/download/{fileId}")
+    public void download(HttpServletRequest request, HttpServletResponse response,
+        @RequestHeader(TokenUtil.TOKEN_HEADER) @NotBlank(message = "Token不能为空") String token,
+        @PathVariable @NotBlank(message = "文件ID不能为空") String fileId) {
+        Download download = this.fileService.download(token, fileId);
+        if (download == null) {
+            response.setCharacterEncoding("UTF-8");
+            response.setContentType("application/json");
+            try {
+                ServletOutputStream outputStream = response.getOutputStream();
+                // TODO改为JSON字符串
+                String content = "下载失效！";
+                outputStream.write(content.getBytes());
+                outputStream.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return;
+        }
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/force-download");
+        try {
+            response.setHeader("Content-Disposition",
+                "attachment; fileName=" + URLEncoder.encode(download.getFileName(), "UTF-8") + ";filename*=utf-8''"
+                    + URLEncoder.encode(download.getFileName(), "UTF-8"));
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
+        byte[] buffer = new byte[1024];
+        try {
+            OutputStream os = response.getOutputStream();
+            int i = download.getInputStream().read(buffer);
+            while (i != -1) {
+                os.write(buffer, 0, i);
+                i = download.getInputStream().read(buffer);
+            }
+            os.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
-	/**
-	 * 获取文件列表
-	 *
-	 * @param token      管理员Token
-	 * @param pageNumber 第几页
-	 * @return 文件列表
-	 */
-	@GetMapping("/file/list/{pageNumber}")
-	public ResultResponseEntity getFileList(@RequestHeader(TokenUtil.TOKEN_HEADER) String token, @PathVariable int pageNumber) {
-		return this.fileService.getFileList(token, pageNumber);
-	}
+    /**
+     * 获取文件列表
+     *
+     * @param token
+     *            管理员Token
+     * @param pageNumber
+     *            第几页
+     * @return 文件列表
+     */
+    @GetMapping("/file/list/{pageNumber}")
+    public ResultResponseEntity getFileList(
+        @RequestHeader(TokenUtil.TOKEN_HEADER) @NotBlank(message = "Token不能为空") String token,
+        @PathVariable @NotNull(message = "分页数不能为空") Integer pageNumber) {
+        return this.fileService.getFileList(token, pageNumber);
+    }
 
-	/**
-	 * 获取文件信息
-	 *
-	 * @param token  管理员Token
-	 * @param fileId 文件ID
-	 * @return 文件信息
-	 */
-	@GetMapping("/file/{fileId}/info")
-	public ResultResponseEntity getFileInfo(@RequestHeader(TokenUtil.TOKEN_HEADER) String token, @PathVariable String fileId) {
-		return this.fileService.getFileInfo(token, fileId);
-	}
+    /**
+     * 获取文件信息
+     *
+     * @param token
+     *            管理员Token
+     * @param fileId
+     *            文件ID
+     * @return 文件信息
+     */
+    @GetMapping("/file/{fileId}/info")
+    public ResultResponseEntity getFileInfo(
+        @RequestHeader(TokenUtil.TOKEN_HEADER) @NotBlank(message = "Token不能为空") String token,
+        @PathVariable @NotBlank(message = "文件ID不能为空") String fileId) {
+        return this.fileService.getFileInfo(token, fileId);
+    }
 
-	/**
-	 * 删除文件
-	 *
-	 * @param token  管理员Token
-	 * @param fileId 文件ID
-	 * @return 是否成功
-	 */
-	@DeleteMapping("/file/{fileId}")
-	public ResultResponseEntity deleteFile(@RequestHeader(TokenUtil.TOKEN_HEADER) String token, @PathVariable String fileId) {
-		return this.fileService.deleteFile(token, fileId);
-	}
+    /**
+     * 删除文件
+     *
+     * @param token
+     *            管理员Token
+     * @param fileId
+     *            文件ID
+     * @return 是否成功
+     */
+    @DeleteMapping("/file/{fileId}")
+    public ResultResponseEntity deleteFile(
+        @RequestHeader(TokenUtil.TOKEN_HEADER) @NotBlank(message = "Token不能为空") String token,
+        @PathVariable @NotBlank(message = "文件ID不能为空") String fileId) {
+        return this.fileService.deleteFile(token, fileId);
+    }
 }
